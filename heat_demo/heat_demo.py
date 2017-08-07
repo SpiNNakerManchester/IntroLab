1097728470f45075661092958636c21c7842b897
Christian Brenninkmeijer <brenninc@cs.man.ac.uk>
2017-08-07 16:14:21 +0100

    cleanup renaming

--- heat_demo/heat_demo.py
+++ heat_demo/heat_demo.py
@@ -6,7 +6,7 @@ from pacman.model.resources.resource_container import ResourceContainer
 from pacman.model.resources.dtcm_resource import DTCMResource
 from pacman.model.resources.sdram_resource import SDRAMResource
 from pacman.model.constraints.placer_constraints\
-    .placer_chip_and_core_constraint import PlacerChipAndCoreConstraint
+    .chip_and_core_constraint import ChipAndCoreConstraint
 from pacman.model.resources.cpu_cycles_per_tick_resource \
     import CPUCyclesPerTickResource
 from pacman.model.resources.iptag_resource import IPtagResource
@@ -117,6 +117,6 @@ Thread(target=read_output, args=[vis_exec, vis_exec.stdout]).start()
 
 for x, y, p in cores:
     heat_demo = HeatDemo()
-    heat_demo.add_constraint(PlacerChipAndCoreConstraint(x, y, p))
+    heat_demo.add_constraint(ChipAndCoreConstraint(x, y, p))
     g.add_machine_vertex_instance(heat_demo)
 g.run(None)
